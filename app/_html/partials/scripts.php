bdc45fd9f475c1ebfc7da9539f2709bf6223f467
Babai <babaev-2010@mail.ru>
2016-02-26 18:20:01 +0300

    start whynot project

---
+++ app/_html/partials/scripts.php
@@ -0,0 +1,24 @@
+<!-- build:js ../scripts/lib.js -->
+<!-- bower:js -->
+<script src="../../bower_components/jquery/dist/jquery.js"></script>
+<script src="../../bower_components/magnific-popup/dist/jquery.magnific-popup.js"></script>
+<script src="../../bower_components/slick-carousel/slick/slick.min.js"></script>
+<script src="../../bower_components/nouislider/distribute/nouislider.js"></script>
+<script src="../../bower_components/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="../../bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js"></script>
+<script src="../../bower_components/air-datepicker/dist/js/datepicker.min.js"></script>
+<script src="../../bower_components/nselect/build/jquery.nselect.min.js"></script>
+<script src="../../bower_components/nselect/build/jquery.mCustomScrollbar.min.js"></script>
+<script src="../../bower_components/jquery-touchswipe/jquery.touchSwipe.js"></script>
+<script src="../../bower_components/jquery-validation/dist/jquery.validate.js"></script>
+<!-- endbower -->
+
+<!-- endbuild -->
+
+<!-- build:js ../scripts/app.js -->
+<!-- inject:js -->
+
+<script src="../scripts/main.js"></script>
+
+<!-- endinject -->
+<!-- endbuild -->
